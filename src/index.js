e9d3f3f31c3032ee1a7f70f824218f49db22ddc5
Stanislav Cherevkov <web--dev@hotmail.com>
2023-01-09 12:05:55 +0200

    feat: add my code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let result = n.toString();
+  result = result.split('');
+  result = result.reverse();
+  result = result.join('');
+
+  return  Number(result);
 }
